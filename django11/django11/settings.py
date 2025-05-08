f18f8efe7f5afb96d206dbf1912dfa98e493f767
Pando4ka-ux <emirgerasimov@yandex.ru>
2025-05-08 17:46:04 +0400

    hh

--- django11/django11/settings.py
+++ django11/django11/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-av7ct-66#-l9rr1)sm69^6b1#3gy(x1b7#cfblg)pin3^10m&$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
