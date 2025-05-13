a751b18b6d83db6efd4c50a236e26dffa200f9f4
Pando4ka-ux <emirgerasimov@yandex.ru>
2025-05-13 17:37:10 +0400

    klog

--- django11/start_django/views.py
+++ django11/start_django/views.py
@@ -22,6 +22,22 @@ def product_detail(request, id, slug):
     })
 
 
+from flask import Flask, request
+import git 
+
+app=Flask(__name__)
+@app.route('/update_server', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('https://github.com/Pando4ka-ux/Shop')
+        origin = repo.remotes.origin
+
+        origin.pull()
+
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
+
 
 
 '''
