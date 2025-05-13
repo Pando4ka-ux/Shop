7f444aa649948f6efdea81fa5275d366429c1a70
Pando4ka-ux <emirgerasimov@yandex.ru>
2025-05-13 17:58:41 +0400

    klog

--- django11/django11/wsgi.py
+++ django11/django11/wsgi.py
@@ -12,8 +12,10 @@ import sys
 
 sys.path.insert(0, '/home/Pando4ka/django11')
 
-from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django11.settings')
 
+
+
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
--- django11/start_django/urls.py
+++ django11/start_django/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import product_list, product_detail
+from .views import webhook
 urlpatterns = [
     path('', product_list, name='product_list'),
     path('category/<slug:category_slug>', product_list, name='product_list_by_category'),
-    path('<int:id>/<slug:slug>/', product_detail, name='product_detail')
+    path('<int:id>/<slug:slug>/', product_detail, name='product_detail'),
+    path('update_server/', webhook, name='webhook'),
 ]
